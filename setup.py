c4e0712a6ed672a4908c61b62d63873d51831d1d
Justin Payne <crashfrog@gmail.com>
2025-06-03 09:30:31 +0700

    un-pin biopython

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(name='SeqSero2S',
     scripts=["bin/deinterleave_fastq.sh","bin/Initial_Conditions_SS2S.py","bin/Initial_Conditions_SS2.py","bin/SeqSero2S.py","bin/SeqSero2_update_kmer_database.py"],
     packages=[""],
     include_package_data = True,
-    install_requires=['biopython==1.73'],
+    install_requires=['biopython~=1.73'],
     data_files=[("seqsero2s_db",["seqsero2s_db/mlst.pickle","seqsero2s_db/antigens.pickle","seqsero2s_db/H_and_O_and_specific_genes.fasta","seqsero2s_db/invA_mers_dict","seqsero2s_db/special.pickle"]),("seqsero2s_db/kmer",["seqsero2s_db/kmer/salmonella_35.txt","seqsero2s_db/kmer/salmonella_config.txt","seqsero2s_db/kmer/salmonella_hemD.tfa","seqsero2s_db/kmer/salmonella.log","seqsero2s_db/kmer/salmonella_purE.tfa","seqsero2s_db/kmer/salmonella_thrA.tfa","seqsero2s_db/kmer/salmonella_aroC.tfa","seqsero2s_db/kmer/salmonella_dnaN.tfa","seqsero2s_db/kmer/salmonella_hisD.tfa","seqsero2s_db/kmer/salmonella_profile.txt","seqsero2s_db/kmer/salmonella_sucA.tfa","seqsero2s_db/kmer/salmonella_weight.txt"])],
     zip_safe=False,
 )
